45538683abddf056ffea347743ea1cfe93413860
Prabhash Jain <jainprabhash7@gmail.com>
2025-06-15 01:57:55 +0530

    add get customer by email API endpoint

--- ragmap-backend/ragmap/src/main/java/com/ragmap/controller/ShopifyController.java
+++ ragmap-backend/ragmap/src/main/java/com/ragmap/controller/ShopifyController.java
@@ -18,8 +18,15 @@ public class ShopifyController {
     public String getProducts() {
         System.out.println("Fetching products from Shopify");
         String products = shopifyService.getProducts();
-        System.out.println("Products fetched: " + products);
-
+        System.out.println("Products fetched: " + products.length());
         return products;
+    }
+
+    @GetMapping("/customers")
+    public String getCustomer(@RequestParam String email) {
+        System.out.println("Fetching customer from Shopify");
+        String customer = shopifyService.getCustomer(email);
+        System.out.println("Customer fetched: " + customer);
+        return customer;
     }
 }
--- ragmap-backend/ragmap/src/main/java/com/ragmap/service/ShopifyService.java
+++ ragmap-backend/ragmap/src/main/java/com/ragmap/service/ShopifyService.java
@@ -7,11 +7,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 public class ShopifyService {
 
-    @Value("${shopify.store}")
-    private String shopifyStore;
+    @Value("${shopify.api.base-url}")
+    private String baseUri;
 
-   /* @Value("${shopify.tkn}")
-    private String tkn;*/
+//    @Value("${shopify.tkn}")
+//    private String tkn;
 
     private final WebClient webClient;
 
@@ -21,8 +21,23 @@ public class ShopifyService {
 
     public String getProducts() {
         return webClient.get()
-                .uri("https://" + shopifyStore+"/admin/api/2023-04/products.json")
-               // .header("X-Shopify-Access-Token", tkn)
+                .uri(baseUri + "/products.json")
+                //.header("X-Shopify-Access-Token", tkn)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    public String getCustomer(String email) {
+        String customerEndPoint = baseUri + "/customers/search.json?query=email:";
+        if (email != null && !email.isEmpty()) {
+            customerEndPoint += email;
+        } else {
+            customerEndPoint += "";
+        }
+        return webClient.get()
+                .uri(customerEndPoint)
+                //.header("X-Shopify-Access-Token", tkn)
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
